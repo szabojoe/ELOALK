df5dfd23eb08b1c945b8f0be9af90b649fd24429
EH6KLB_SzJ <>
2024-05-08 17:59:37 +0200

    valotozok

--- Csigaverseny/src/csigaverseny/Jatek.java
+++ Csigaverseny/src/csigaverseny/Jatek.java
@@ -7,10 +7,16 @@ public class Jatek {
     }
 
     public void start() {
-        
+        boolean csiga1Nyert,csiga2Nyert,csiga3Nyert,fogadasNyert=false;
+        int cs1Tav=0,cs2Tav=0,cs3Tav=0,nyertesTav=0;
+        int lep1,lep2,lep3;
+                
         Csiga cs1 = new Csiga();
         Csiga cs2 = new Csiga();
         Csiga cs3 = new Csiga();
+        
+ 
+        
     }
     
 }
