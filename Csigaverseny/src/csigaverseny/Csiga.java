02a39968dcfdc077f165293b74aff1582215b931
EH6KLB_SzJ <>
2024-05-08 17:39:44 +0200

    gyorsitasi lehetoseg: ha a csiga kapott ilyet akkor duplázodik a leptetese

--- Csigaverseny/src/csigaverseny/Csiga.java
+++ Csigaverseny/src/csigaverseny/Csiga.java
@@ -6,8 +6,13 @@ class Csiga {
     private static final int MAX = 4;
     private static Random rnd = new Random();
     private int szam;
+    private int gyorsito=1;
     
     public void lep(){
         szam = Csiga.rnd.nextInt(Csiga.MAX);
+    }
+    
+    public int getLep(){
+        return szam*gyorsito;
     }
 }
