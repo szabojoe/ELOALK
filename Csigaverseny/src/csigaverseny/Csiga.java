6442161004598e2bc79bebc78b8ea721294260f8
EH6KLB_SzJ <>
2024-05-08 17:31:29 +0200

    csiga leptet: 4 lehetőség: 0...3 a leptetes rnd-vel generalva

--- Csigaverseny/src/csigaverseny/Csiga.java
+++ Csigaverseny/src/csigaverseny/Csiga.java
@@ -1,5 +1,13 @@
 package csigaverseny;
 
+import java.util.Random;
+
 class Csiga {
+    private static final int MAX = 4;
+    private static Random rnd = new Random();
+    private int szam;
     
+    public void lep(){
+        szam = Csiga.rnd.nextInt(Csiga.MAX);
+    }
 }
